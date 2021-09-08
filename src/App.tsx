ffa47264f6f217b1d58d29846baef35a65bcee28
Marc Diaz <marcgabe15@gmail.com>
2021-09-08 18:00:10 -0400

    prettier and linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 function App() {
   return (
     <div className="App">
