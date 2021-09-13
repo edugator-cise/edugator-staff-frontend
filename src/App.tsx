abe2610de91e6927a44e8d268beed28454ebe6ba
Tanner Ropp <tanner.ropp@ufl.edu>
2021-09-13 18:26:13 -0400

    build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu'
-import React from 'react';
-import './App.css';
+import { Typography } from "@material-ui/core";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Typography variant="h1" color="textSecondary">INSERT EDUGATOR HERE</Typography>
+      <Typography variant="h1" color="textSecondary">
+        INSERT EDUGATOR HERE
+      </Typography>
     </div>
   );
 }
