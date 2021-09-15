ad93149e9f4b608cf9eb04de632940c466575d76
joshuadargan <joshuadargan282@ufl.edu>
2021-09-15 17:09:37 -0400

    root reducer prettier

--- src/app/common/rootReducer.ts
+++ src/app/common/rootReducer.ts
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux';
-import authReducer from '../auth/auth.reducer';
+import { combineReducers } from "redux";
+import authReducer from "../auth/auth.reducer";
 /* import slices of state here */
 
 /* Place the object keys for state here followed by the reducer taken from that slice
