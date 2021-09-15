9a92b05f01c8e0da3f61338068259019bb90ae85
joshuadargan <joshuadargan282@ufl.edu>
2021-09-15 19:53:25 -0400

    Login page example

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -1,7 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../common/store";
+import { requestLogin } from "./auth.actions";
+
 export function LoginPage(): React.ReactElement {
+	const dispatch = useDispatch();
+	const authState = useSelector((state: RootState) => state.auth);
+
 	return (
 		<div>
 			<h1>login</h1>
+			<p>isLoading: {authState.isLoading ? "True" : "False"}</p>
+			<p>errorMessage: {authState.errorMessage}</p>
+			<p>token: {authState.authorizationToken}</p>
+			<button onClick={() => dispatch(requestLogin("test", "test"))}>
+				Login Test
+			</button>
 		</div>
 	);
 }
