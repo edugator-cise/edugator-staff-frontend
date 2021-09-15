c04f7d24ca6e68af5bef9bcdaa8f568afe45ffdf
joshuadargan <joshuadargan282@ufl.edu>
2021-09-15 14:12:06 -0400

    created actions

---
+++ src/app/auth/auth.actions.ts
@@ -0,0 +1,18 @@
+import { IGenericAction } from "../common/types";
+
+export enum AuthActionTypes {
+    REQUEST_LOGIN = "Request_Login"
+}
+
+export interface IRequestLoginAction extends IGenericAction {
+    username: string,
+    password: string
+}
+
+export function requestLogin(username: string, password: string) : IRequestLoginAction {
+  return { 
+    type: AuthActionTypes.REQUEST_LOGIN,
+    username,
+    password
+  }
+}
