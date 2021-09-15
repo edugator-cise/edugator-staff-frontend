81ac6b909ed1bdaefc8e1612eb799a62840ff100
joshuadargan <joshuadargan282@ufl.edu>
2021-09-15 17:44:44 -0400

    improving types

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,21 +1,46 @@
-import { IGenericAction } from "../common/types";
-
 export enum AuthActionTypes {
-  REQUEST_LOGIN = "Request_Login",
+	REQUEST_LOGIN = "RequestLogin",
+	RECEIVE_LOGIN_SUCCESS = "ReceiveLoginSuccess",
+	RECEIVE_LOGIN_FAILURE = "ReceiveLoginFailure",
 }
 
-export interface IRequestLoginAction extends IGenericAction {
-  username: string;
-  password: string;
+export interface IRequestLoginAction {
+	type: AuthActionTypes.REQUEST_LOGIN;
+	username: string;
+	password: string;
+}
+
+export interface IReceiveLoginSuccessAction {
+	type: AuthActionTypes.RECEIVE_LOGIN_SUCCESS;
+	token: string;
+}
+
+export interface IReceiveLoginFailureAction {
+	type: AuthActionTypes.RECEIVE_LOGIN_FAILURE;
+	message: string;
 }
 
 export function requestLogin(
-  username: string,
-  password: string
+	username: string,
+	password: string
 ): IRequestLoginAction {
-  return {
-    type: AuthActionTypes.REQUEST_LOGIN,
-    username,
-    password,
-  };
+	return {
+		type: AuthActionTypes.REQUEST_LOGIN,
+		username,
+		password,
+	};
+}
+
+export function receiveLoginSuccess(token: string): IReceiveLoginSuccessAction {
+	return {
+		type: AuthActionTypes.RECEIVE_LOGIN_SUCCESS,
+		token,
+	};
+}
+
+export function receiveLoginFailure(token: string): IReceiveLoginSuccessAction {
+	return {
+		type: AuthActionTypes.RECEIVE_LOGIN_SUCCESS,
+		token,
+	};
 }
--- src/app/auth/auth.reducer.ts
+++ src/app/auth/auth.reducer.ts
@@ -1,34 +1,47 @@
-import { IGenericAction } from "../common/types";
-import { AuthActionTypes } from "./auth.actions";
+import {
+	AuthActionTypes,
+	IReceiveLoginFailureAction,
+	IReceiveLoginSuccessAction,
+	IRequestLoginAction,
+} from "./auth.actions";
 
 export interface IAuthState {
-  authorizationToken: string;
-  isLoading: boolean;
+	authorizationToken: string;
+	isLoading: boolean;
 }
 
 const initialAuthState: IAuthState = {
-  authorizationToken: "",
-  isLoading: false,
+	authorizationToken: "",
+	isLoading: false,
 };
 
 /**
  * Gets a safe copy of the initial auth state
  */
 export function getInitialAuthState(): IAuthState {
-  return { ...initialAuthState };
+	return { ...initialAuthState };
 }
 
 export default function authReducer(
-  state = getInitialAuthState(),
-  action: IGenericAction
+	state = getInitialAuthState(),
+	action:
+		| IReceiveLoginSuccessAction
+		| IReceiveLoginFailureAction
+		| IRequestLoginAction
 ): IAuthState {
-  switch (action.type) {
-    case AuthActionTypes.REQUEST_LOGIN:
-      return {
-        ...state,
-        isLoading: true,
-      };
-    default:
-      return { ...state };
-  }
+	switch (action.type) {
+		case AuthActionTypes.REQUEST_LOGIN:
+			return {
+				...state,
+				isLoading: true,
+			};
+		case AuthActionTypes.RECEIVE_LOGIN_SUCCESS:
+			return {
+				...state,
+				isLoading: false,
+				authorizationToken: action.token,
+			};
+		default:
+			return { ...state };
+	}
 }
