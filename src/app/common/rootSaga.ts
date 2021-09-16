f02f4f4125b3df4f4c4483155a01894edf054cd1
joshuadargan <joshuadargan282@ufl.edu>
2021-09-15 20:34:05 -0400

    renaming auth to login

--- src/app/common/rootSaga.ts
+++ src/app/common/rootSaga.ts
@@ -1,7 +1,7 @@
 import { all } from "redux-saga/effects";
-import authSaga from "../Login/Login.sagas";
+import loginSaga from "../Login/Login.sagas";
 
 /** Place additional saga functions in the array */
 export default function* rootSaga() {
-  yield all([authSaga()]);
+	yield all([loginSaga()]);
 }
