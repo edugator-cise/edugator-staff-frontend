7c06c6d992be133adf3f9981f29c85fdec3de6d3
Tanner Ropp <tanner.ropp@ufl.edu>
2021-09-17 01:28:39 -0400

    added basic nav buttons

--- src/features/ProblemEditorContainer/ProblemEditorContainer.tsx
+++ src/features/ProblemEditorContainer/ProblemEditorContainer.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Step, StepLabel, Stepper } from "@material-ui/core";
+import { Box, Button, Card, Step, StepLabel, Stepper } from "@material-ui/core";
 import React, { useState } from "react";
 
 const steps = [
@@ -11,6 +11,14 @@ const steps = [
 
 export const ProblemEditorContainer = () => {
   const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
 
   return (
     <Box display="flex" flexDirection="column" flexGrow={1}>
@@ -27,8 +35,26 @@ export const ProblemEditorContainer = () => {
           );
         })}
       </Stepper>
-      <Box border="1px solid lightgray" borderRadius="10px" flexGrow={1}>
-        Content
+      <Box
+        border="1px solid lightgray"
+        borderRadius="10px"
+        flexGrow={1}
+        padding="1rem"
+        display="flex"
+        flexDirection="Column"
+      >
+        Content goes here
+        <Box marginTop="auto" display="flex" justifyContent="space-between">
+          <Button onClick={handleBack} disabled={activeStep === 0}>
+            Back
+          </Button>
+          <Button
+            onClick={handleNext}
+            disabled={activeStep === steps.length - 1}
+          >
+            Next
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
