3a68525d1065b42e42818059d1b2f78da6e0991a
Adrian Salazar <44170778+AqSlz@users.noreply.github.com>
2021-09-21 15:34:00 -0400

    Adding Typography import to App.tsx
    Necessary commit since my PR was done before the fix PR. With this added, there shouldn't be any merge conflicts with main.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import { LoginPage } from "./app/Login/Login";
+import { Typography } from "@material-ui/core";
 
 
 function App() {
   return (
     <div className="App">
-      <LoginPage></LoginPage>
+      <Typography variant="h3" color="textSecondary">
+        <LoginPage></LoginPage>
+      </Typography>
     </div>
   );
 }
