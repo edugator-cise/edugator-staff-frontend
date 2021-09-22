2791a85b8e733b856c3a15ce464d9ac2ad820ec2
Tanner Ropp <tanner.ropp@ufl.edu>
2021-09-21 20:14:10 -0400

    added comments

--- src/pages/ProblemEditor/ProblemEditorContainer/problemEditorContainerSlice.ts
+++ src/pages/ProblemEditor/ProblemEditorContainer/problemEditorContainerSlice.ts
@@ -1,6 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface ProblemEditorContainerState {
+  /* 
+    Each of these represents a stage of the problem editor Stepper component.
+    The validity of each subform will be used to control/limit the navigation
+    of the stepper. Each form can perform it's own validation then set these 
+    fields accordingly 
+  */
   metadataIsValid: boolean;
   problemIsValid: boolean;
   codeIsValid: boolean;
