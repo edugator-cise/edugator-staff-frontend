1504aad710c3debf20b7b46eea0f1f4d29552b62
joshuadargan <joshuadargan282@ufl.edu>
2021-09-25 22:40:50 -0400

    updated the routes

--- src/shared/PrivateRoute.tsx
+++ src/shared/PrivateRoute.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { Route, Redirect, RouteProps } from "react-router-dom";
-import { connect, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Routes } from "./Routes.constants";
 import { RootState } from "../app/common/store";
 
 interface PrivateRouteProps {
-	ChildComponent: React.ReactNode;
-	routerProps: RouteProps;
+	routeProps: RouteProps;
 }
 
-export function PrivateRoute(props: PrivateRouteProps): React.ReactElement {
-	const { ChildComponent, routerProps } = props;
+//Reference: https://reactrouter.com/web/example/auth-workflow
+export const PrivateRoute: React.FC<PrivateRouteProps> = ({
+	children,
+	...routeProps
+}) => {
 	const authState = useSelector((state: RootState) => state.login);
 	return (
 		<Route
-			{...routerProps}
+			{...routeProps}
 			render={({ location }) =>
 				authState.authorizationToken ? (
-					ChildComponent
+					children
 				) : (
 					<Redirect
 						to={{
@@ -29,6 +31,4 @@ export function PrivateRoute(props: PrivateRouteProps): React.ReactElement {
 			}
 		/>
 	);
-}
-
-export default connect(PrivateRoute);
+};
--- src/shared/Routes.constants.tsx
+++ src/shared/Routes.constants.tsx
@@ -1,3 +1,5 @@
 export enum Routes {
 	Login = "/login",
+	Modules = "/modules",
+	ProblemEditor = "/problem/edit",
 }
