3a029bef4b729a277c9593cd5d31222a85b66b27
joshuadargan <joshuadargan282@ufl.edu>
2021-09-25 20:23:44 -0400

    added default routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import "./App.css";
 import { LoginPage } from "./app/Login/Login";
 import { Typography } from "@material-ui/core";
+import { Route, Switch } from "react-router-dom";
+import { Routes } from "./shared/Routes.constants";
 
 function App() {
-  return (
-    <div className="App">
-      <Typography variant="h3" color="textSecondary">
-        <LoginPage></LoginPage>
-      </Typography>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Typography variant="h3" color="textSecondary">
+				<Switch>
+					<Route path={Routes.Login}>
+						<LoginPage />
+					</Route>
+				</Switch>
+			</Typography>
+		</div>
+	);
 }
 
 export default App;
