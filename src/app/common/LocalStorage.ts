1ce4decc647b1863791671bf22fd9d0201e990ca
joshuadargan <joshuadargan282@ufl.edu>
2021-09-27 19:37:07 -0400

    local storage wrapper

---
+++ src/app/common/LocalStorage.ts
@@ -0,0 +1,24 @@
+/** Wrapper class for the window.localStorage that enables access to its methods */
+export class LocalStorage {
+	static jwtToken = "jwtToken";
+
+	/** Returns the current value associated with the token,
+	 * or null if the token does not exist. */
+	static getToken(): string | null {
+		return localStorage.getItem(this.jwtToken);
+	}
+
+	/**
+	 * Sets the jwtToken to the token value, creating a new key/value pair if none existed for key previously.
+	 * Throws a "QuotaExceededError" DOMException exception if the new value couldn't be set.
+	 * (Setting could fail if, e.g., the user has disabled storage for the site, or if the quota has been exceeded.)
+	 */
+	static setToken(token: string): void {
+		localStorage.setItem(this.jwtToken, token);
+	}
+
+	/** Deletes the token if it exists safely */
+	static removeToken(): void {
+		localStorage.removeItem(this.jwtToken);
+	}
+}
