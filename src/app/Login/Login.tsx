091a06a57f5e6f81521e90d16f174143dac06913
joshuadargan <joshuadargan282@ufl.edu>
2021-09-30 10:35:27 -0400

    fixing login styles to mui

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -1,12 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../common/store";
 import { requestLogin } from "./Login.slice";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import Button from "@material-ui/core/Button";
-import { CircularProgress } from "@material-ui/core";
 import { IRequestLoginAction } from "./Login.slice";
 import { Field, Form, Formik } from "formik";
 import { FormTextField } from "../../shared/FormTextField";
@@ -14,99 +8,109 @@ import { LayoutContainer } from "../../shared/LayoutContainer";
 import { Routes } from "../../shared/Routes.constants";
 import { Redirect } from "react-router";
 import { LocalStorage } from "../common/LocalStorage";
+import makeStyles from "@mui/styles/makeStyles";
+import createStyles from "@mui/styles/createStyles";
+import {
+	Button,
+	Card,
+	CardActions,
+	CardContent,
+	CircularProgress,
+	Theme,
+} from "@mui/material";
 
 //Refernce: https://github.com/creativesuraj/react-material-ui-login/blob/master/src/components/Login.tsx
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    container: {
-      display: "grow",
-      flexWrap: "wrap",
-      alignItems: "center",
-      justifyContent: "center",
-      width: "50%",
-      margin: `${theme.spacing(0)} auto`,
-      left: "50%",
-      top: "50%",
-    },
-    loginBtn: {
-      marginTop: theme.spacing(2),
-      flexGrow: 1,
-    },
-    card: {
-      marginTop: theme.spacing(10),
-    },
-  })
+	createStyles({
+		container: {
+			display: "grow",
+			flexWrap: "wrap",
+			alignItems: "center",
+			justifyContent: "center",
+			width: "50%",
+			margin: `${theme.spacing(0)} auto`,
+			left: "50%",
+			top: "50%",
+		},
+		loginBtn: {
+			marginTop: theme.spacing(2),
+			flexGrow: 1,
+		},
+		card: {
+			marginTop: theme.spacing(10),
+		},
+	})
 );
 
 export function LoginPage(): React.ReactElement {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-  const authState = useSelector((state: RootState) => state.login);
-  return LocalStorage.getToken() ? (
-    <Redirect
-      to={{
-        pathname: Routes.Modules,
-        state: { from: Routes.Login },
-      }}
-    />
-  ) : (
-    <LayoutContainer pageTitle="Admin Login">
-      <div>
-        {authState.isLoading && <CircularProgress />}
-        {
-          //TODO: Temp until we solve Alert MUI Issue
-        }
-        <Formik
-          initialValues={{ username: "", password: "" }}
-          onSubmit={(values: IRequestLoginAction) => {
-            dispatch(requestLogin(values));
-          }}
-        >
-          {() => (
-            <Form className={classes.container}>
-              <Card className={classes.card}>
-                <CardContent>
-                  <div>
-                    <Field
-                      name="username"
-                      label="Username"
-                      placeholder="Username"
-                      margin="normal"
-                      fullWidth
-                      required
-                      component={FormTextField}
-                    />
-                  </div>
-                  <div>
-                    <Field
-                      name="password"
-                      placeholder="Password"
-                      label="Password"
-                      type="password"
-                      margin="normal"
-                      fullWidth
-                      required
-                      component={FormTextField}
-                    />
-                  </div>
-                </CardContent>
-                {authState.errorMessage && <p>{authState.errorMessage}</p>}
-                <CardActions>
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    size="large"
-                    color="secondary"
-                    className={classes.loginBtn}
-                  >
-                    Submit
-                  </Button>
-                </CardActions>
-              </Card>
-            </Form>
-          )}
-        </Formik>
-      </div>
-    </LayoutContainer>
-  );
+	const classes = useStyles();
+	const dispatch = useDispatch();
+	const authState = useSelector((state: RootState) => state.login);
+	return LocalStorage.getToken() ? (
+		<Redirect
+			to={{
+				pathname: Routes.Modules,
+				state: { from: Routes.Login },
+			}}
+		/>
+	) : (
+		<LayoutContainer pageTitle="Admin Login">
+			<div>
+				{authState.isLoading && <CircularProgress />}
+				{
+					//TODO: Temp until we solve Alert MUI Issue
+				}
+				<Formik
+					initialValues={{ username: "", password: "" }}
+					onSubmit={(values: IRequestLoginAction) => {
+						dispatch(requestLogin(values));
+					}}
+				>
+					{() => (
+						<Form className={classes.container}>
+							<Card className={classes.card}>
+								<CardContent>
+									<div>
+										<Field
+											name="username"
+											label="Username"
+											placeholder="Username"
+											margin="normal"
+											fullWidth
+											required
+											component={FormTextField}
+										/>
+									</div>
+									<div>
+										<Field
+											name="password"
+											placeholder="Password"
+											label="Password"
+											type="password"
+											margin="normal"
+											fullWidth
+											required
+											component={FormTextField}
+										/>
+									</div>
+								</CardContent>
+								{authState.errorMessage && <p>{authState.errorMessage}</p>}
+								<CardActions>
+									<Button
+										type="submit"
+										variant="contained"
+										size="large"
+										color="secondary"
+										className={classes.loginBtn}
+									>
+										Submit
+									</Button>
+								</CardActions>
+							</Card>
+						</Form>
+					)}
+				</Formik>
+			</div>
+		</LayoutContainer>
+	);
 }
