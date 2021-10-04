78fc4bed861655c430649160cff52bb287289f93
joshuadargan <joshuadargan282@ufl.edu>
2021-10-04 15:37:31 -0400

    emotion wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { createTheme } from "@mui/material";
-import { ThemeProvider, StyledEngineProvider } from "@mui/system";
+import {
+	ThemeProvider as MuiThemeProvider,
+	StyledEngineProvider,
+} from "@mui/system";
 import React from "react";
 import "./App.css";
 import { LoginPage } from "./pages/Login/LoginPage";
@@ -8,27 +11,30 @@ import { Routes } from "./shared/Routes.constants";
 import { PrivateRoute } from "./shared/PrivateRoute";
 import Modules from "./pages/modules/ModulesView";
 import { ProblemEditorPage } from "./pages/ProblemEditor/ProblemEditorPage";
+import { ThemeProvider } from "@emotion/react";
 
 function App() {
 	const theme = createTheme();
 
 	return (
 		<StyledEngineProvider injectFirst>
-			<ThemeProvider theme={theme}>
-				<div className="App">
-					<Switch>
-						<Route exact path={Routes.Login}>
-							<LoginPage />
-						</Route>
-						<PrivateRoute exact path={Routes.ProblemEditor}>
-							<ProblemEditorPage />
-						</PrivateRoute>
-						<PrivateRoute exact path={Routes.Modules}>
-							<Modules />
-						</PrivateRoute>
-					</Switch>
-				</div>
-			</ThemeProvider>
+			<MuiThemeProvider theme={theme}>
+				<ThemeProvider theme={theme}>
+					<div className="App">
+						<Switch>
+							<Route exact path={Routes.Login}>
+								<LoginPage />
+							</Route>
+							<PrivateRoute exact path={Routes.ProblemEditor}>
+								<ProblemEditorPage />
+							</PrivateRoute>
+							<PrivateRoute exact path={Routes.Modules}>
+								<Modules />
+							</PrivateRoute>
+						</Switch>
+					</div>
+				</ThemeProvider>
+			</MuiThemeProvider>
 		</StyledEngineProvider>
 	);
 }
