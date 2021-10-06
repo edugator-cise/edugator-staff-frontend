e56a7172840fefe21d003d7211d2e35fdcfd9ba0
Tanner Ropp <tanner.ropp@ufl.edu>
2021-10-06 12:54:40 -0400

    moved validation handler into example validator

--- src/pages/ProblemEditor/ProblemEditorContainer/ExampleValidator.tsx
+++ src/pages/ProblemEditor/ProblemEditorContainer/ExampleValidator.tsx
@@ -1,15 +1,46 @@
 import { Box, FormControlLabel, FormGroup, Switch } from "@mui/material";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "../../../app/common/hooks";
+import {
+  validateCode,
+  validateMetadata,
+  validateProblem,
+  validateServerConfig,
+  validateTestEditor,
+} from "./problemEditorContainerSlice";
 
 interface Props {
-  handleValidation: (isValid: boolean) => void;
   validationStatus: () => boolean;
 }
 
-export const ExampleValidator = ({
-  handleValidation,
-  validationStatus,
-}: Props) => {
+export const ExampleValidator = ({ validationStatus }: Props) => {
+  const dispatch = useDispatch();
+  const activeStep = useAppSelector(
+    (state) => state.problemEditorContainer.activeStep
+  );
+
+  /* Dispatches the correct action based on the current active step */
+  const handleValidation = (isValid: boolean): void => {
+    switch (activeStep) {
+      case 0:
+        dispatch(validateMetadata(isValid));
+        break;
+      case 1:
+        dispatch(validateProblem(isValid));
+        break;
+      case 2:
+        dispatch(validateCode(isValid));
+        break;
+      case 3:
+        dispatch(validateServerConfig(isValid));
+        break;
+      case 4:
+        dispatch(validateTestEditor(isValid));
+        break;
+    }
+  };
+
   return (
     <Box mt="auto" ml="auto" mr="auto">
       <FormGroup>
