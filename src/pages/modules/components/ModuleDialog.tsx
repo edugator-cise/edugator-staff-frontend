1530c162622f0d51c412cf07aa92e20daca87773
Adrian Salazar <44170778+AqSlz@users.noreply.github.com>
2021-10-12 15:52:45 -0400

    Getting modules from database and adding modules from frontend (#52)
    * Refactor, Redux, and Redux-Sagas
    
    Variables such as Modules are available on Redux. Redux-Sagas is not being called yet. I need to see what I am missing.
    
    * Minor Fixes
    
    Everything from previous PR is now in Redux-Sagas. There is an error in the console regarding the dialog but it seems it will get fixed if we upgrade to mui
    
    * Upgrade to mui
    
    MUI imports and some linting changes
    
    * MUI Migration complete
    
    Everything looks the same to me.
    
    * Minor Changes to adding a Module
    
    Now there is a textfield for module number
    
    * Updated routes for requests
    
    * Getting modules from the database
    
    * Refactoring
    
    * More refactoring
    
    * Module Dialog supports Adding and Editing
    
    I generalized ModuleDialog display different messages depending if we are intending to add or edit a module. But editing is not yet enabled
    
    * Messages display after action
    
    Used a snackbar to display the latest action's result
    
    * Action feedback improved
    
    Snackbar is smarter about the message, the type of alert, and when to display it.
    
    * Merge complete, adding a module works
    
    Users are not being logged out yet
    
    * Made auth token for requests persistent
    
    The method to set the token in axios only worked until the page was closed or refreshed. With this, now I can come back later and it still works. But this solution is not final.
    
    * Added testing for module state
    
    I finished the things I had left to do for this PR, now all the console.logs should be gone, please tell me if there are any remaining.

---
+++ src/pages/modules/components/ModuleDialog.tsx
@@ -0,0 +1,142 @@
+import React from "react";
+import {
+  Button,
+  TextField,
+  TextFieldProps,
+  DialogTitle,
+  Dialog,
+  Paper,
+  Divider,
+  DialogContentText,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { INewModule, DialogStatus } from "../../../shared/types";
+
+import { useAppDispatch } from "../../../app/common/hooks";
+import { requestNewModule } from "../ModulesPage.slice";
+
+const Form = styled("div")({
+  display: "flex",
+});
+
+const NumberField = styled(TextField)<TextFieldProps>(({ theme }) => ({
+  minWidth: "11%",
+  maxWidth: "13%",
+  margin: theme.spacing(1),
+  marginLeft: theme.spacing(3),
+}));
+
+const NameTextField = styled(TextField)<TextFieldProps>(({ theme }) => ({
+  margin: theme.spacing(1),
+  maxWidth: "70%",
+}));
+
+const DialogContent = styled(DialogContentText)(({ theme }) => ({
+  margin: theme.spacing(1),
+  marginLeft: theme.spacing(3),
+  marginRight: theme.spacing(3),
+}));
+
+const AddButton = styled(Button)(({ theme }) => ({
+  margin: theme.spacing(1),
+}));
+
+const Footer = styled("div")({
+  float: "right",
+});
+
+const dialogTitle = (status: DialogStatus) => {
+  if (status === DialogStatus.CREATE) {
+    return "Creating a new module";
+  } else if (status === DialogStatus.EDIT) {
+    return "Editing an existing module";
+  } else {
+    return "Closing this dialog";
+  }
+};
+
+export interface ModuleDialogProps {
+  open: boolean;
+  moduleValues: INewModule;
+  dialogOperation: DialogStatus;
+  handleClose: () => void;
+  moduleValuesInput: (input: INewModule) => void;
+}
+
+export function ModuleDialog(props: ModuleDialogProps) {
+  const {
+    open,
+    moduleValues,
+    dialogOperation,
+    handleClose,
+    moduleValuesInput,
+  } = props;
+
+  const dispatch = useAppDispatch();
+
+  const handleDialogSubmit = () => {
+    if (dialogOperation === DialogStatus.CREATE) {
+      dispatch(
+        requestNewModule({
+          moduleName: moduleValues.nameInput,
+          moduleNum: moduleValues.numberInput,
+        })
+      );
+    } else if (dialogOperation === DialogStatus.EDIT) {
+      // dispatch rename module
+    }
+
+    // TODO:
+    //  dont close before checking
+    //  if action was successful
+    handleClose();
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open} maxWidth="sm" fullWidth>
+      <Paper elevation={3}>
+        <DialogTitle id="module-title-dialog">
+          {dialogTitle(dialogOperation)}
+        </DialogTitle>
+        <Divider />
+        <DialogContent>
+          To add a new module, or to modify a module's name and number, please
+          use the form below.
+        </DialogContent>
+        <Form>
+          <NumberField
+            label="Number"
+            variant="filled"
+            inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+            onChange={(event) => {
+              moduleValuesInput({
+                ...moduleValues,
+                numberInput: parseInt(event.target.value),
+              });
+            }}
+            fullWidth
+            focused
+          />
+          <NameTextField
+            label="Module Name"
+            variant="filled"
+            onChange={(event) => {
+              moduleValuesInput({
+                ...moduleValues,
+                nameInput: event.target.value,
+              });
+            }}
+            fullWidth
+            focused
+          />
+        </Form>
+
+        <Footer>
+          <AddButton onClick={() => handleDialogSubmit()} variant="outlined">
+            Add module
+          </AddButton>
+        </Footer>
+      </Paper>
+    </Dialog>
+  );
+}
---
+++ src/pages/modules/config.ts
@@ -0,0 +1,69 @@
+import { AxiosRequestConfig } from "axios";
+import { LocalStorage } from "../../app/common/LocalStorage";
+
+interface IApiConfig {
+  [desc: string]: AxiosRequestConfig;
+}
+
+const adminAPI = "https://edugator-admin.com"; // => base
+const token = LocalStorage.getToken();
+
+const noCache = { "Cache-Control": "no-cache" };
+const auth = { Authorization: "Bearer " + token };
+
+const apiUrls: IApiConfig = {
+  "get modules and problems": {
+    url: adminAPI + "/v1/module/WithProblems",
+    method: "GET",
+    headers: {
+      ...noCache,
+      ...auth, // because previous way is gone on refresh
+    },
+  },
+  "add module": {
+    url: adminAPI + "/v1/module",
+    method: "POST",
+    headers: {
+      ...noCache,
+      ...auth,
+    },
+  },
+  "modify module": {
+    url: adminAPI + "/v1/module",
+    method: "PUT",
+    headers: {
+      ...noCache,
+      ...auth,
+    },
+  },
+  "delete module": {
+    url: adminAPI + "/v1/module",
+    method: "DELETE",
+    headers: {
+      ...noCache,
+      ...auth,
+    },
+  },
+};
+
+interface ISnackBarConfig {
+  [type: string]: string;
+}
+
+/* Needs to be the same as modules reducer
+ * remember to update as necessary
+ * format: reducerName/actionName
+ * ex: modules/requestModulesSuccess
+ * TODO:
+ *  Take error code into consideration
+ *  Take error message into consideration
+ */
+
+export const AlertMsg: ISnackBarConfig = {
+  "modules/requestModulesSuccess": "Modules received successfully",
+  "modules/requestModulesFailure": "Failed to get modules from database",
+  "modules/requestNewModuleSuccess": "Module created successfully",
+  "modules/requestNewModuleFailure": "Module creation failed",
+};
+
+export default apiUrls;
