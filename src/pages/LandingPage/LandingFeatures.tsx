895b37f7e235a2a3e167a69742d72163c0d2ba48
digitdustin <dustinkarp52@gmail.com>
2021-10-15 12:01:48 -0400

    updated features layout

--- src/pages/LandingPage/LandingFeatures.tsx
+++ src/pages/LandingPage/LandingFeatures.tsx
@@ -1,24 +1,16 @@
 import {
-  Container,
-  Toolbar,
   Typography,
-  Button,
-  Avatar,
-  Menu,
-  MenuItem,
-  Grid,
-  Fade,
-  Grow,
-  Slide,
 } from "@mui/material";
 import React from "react";
 import { styled } from "@mui/styles";
-import FeaturesBG from '../../assets/FeaturesBG.svg'
 import theme from "../../shared/theme";
-import { MenuBook, Brush, Code } from '@mui/icons-material/';
+import FeaturesBG from '../../assets/FeaturesBG.svg'
+import Diamond from '../../assets/icons8-diamond-100.png'
+import FlipChart from '../../assets/icons8-flip-chart-100.png'
+import Glasses from '../../assets/icons8-glasses-100.png'
 
 const Holder = styled("div")({
-  height: 850,
+  height: 800,
   width: "100%",
   backgroundColor: "red",
   display: "flex",
@@ -40,31 +32,39 @@ const Holder = styled("div")({
 const FeatureHolder = styled("div")({
   height: '55%',
   width: '70%',
+  maxWidth: 1100,
+  minWidth: 1000,
   display: 'flex',
   flexDirection: 'row',
   justifyContent: 'space-between',
-  alignItems: 'center',
+  alignItems: 'flex-end',
 })
 
 const Feature = styled('div')({
-  height: '80%',
+  height: '70%',
   width: 300,
   borderRadius: 15,
   display: 'flex',
+  position: 'relative',
   flexDirection: 'column',
   alignItems: 'center',
-  backgroundColor: 'white',
+  justifyContent: 'center',
+  background: 'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), transparent)',
   borderTop: '5px solid ' + theme.palette.primary.main,
+  //borderBottom: '5px solid ' + theme.palette.secondary.light,
 })
 
 const IconHolder = styled('div')({
-  height: 120,
-  width: 120,
-  borderRadius: 15,
-  backgroundColor: theme.palette.primary.light,
+  height: 150,
+  width: 150,
+  position: 'absolute',
+  top: -80,
+  borderRadius: 75,
+  backgroundColor: 'white',
   display: 'flex',
   justifyContent: 'center',
-  alignItems: 'center'
+  alignItems: 'center',
+  borderBottom: '5px solid ' + theme.palette.primary.main,
 })
 
 function LandingFeatures() {
@@ -72,32 +72,34 @@ function LandingFeatures() {
     <Holder>
         <Typography variant="h3" color="white">Start your journey learning</Typography>
         <Typography variant="h3" color="primary">Computer Science</Typography>
-        <Typography variant="body1" color="white" sx={{marginTop: 3}}>Understand, analyze, and apply Data Structures and Algorithms in your everyday life.</Typography>
+        <Typography variant="body1" color="white" sx={{marginTop: '2em'}}>Understand, analyze, and apply Data Structures and Algorithms in your everyday life.</Typography>
       <FeatureHolder>
         <Feature>
           <IconHolder>
-            <Typography color="primary.dark">
-              <MenuBook style={{fontSize: 60}}/>
+            <Typography color="white">
+              <img src={Glasses} width={100} height={100}/>
             </Typography>
           </IconHolder>
-          <Typography variant="h5"><b>Built for Students</b></Typography>
-
+          <Typography variant="h5" color="white" sx={{maxWidth: 200, marginTop: '0.5em', marginBottom: '0.5em'}}><b>Built for students</b></Typography>
+          <Typography variant="body2" color="white" sx={{maxWidth: 250}}>Edugator is built to make your life easier. Modules, sample problems, documentation, and more!</Typography>
         </Feature>
         <Feature>
           <IconHolder>
-            <Typography color="primary.dark">
-              <Brush style={{fontSize: 60}}/>
+            <Typography color="white">
+              <img src={Diamond} width={100} height={100}/>
             </Typography>
           </IconHolder>
-          <Typography variant="h5"><b>Designed to be Modern</b></Typography>
+          <Typography variant="h5" color="white" sx={{maxWidth: 200, marginTop: '0.5em', marginBottom: '0.5em'}}><b>Designed to be modern</b></Typography>
+          <Typography variant="body2" color="white" sx={{maxWidth: 250}}>Designed with the latest design trends in mind. Edugator feels modern, minimal, and beautiful.</Typography>
         </Feature>
         <Feature>
           <IconHolder>
-            <Typography color="primary.dark">
-              <Code style={{fontSize: 60}}/>
+            <Typography color="white">
+              <img src={FlipChart} width={100} height={100}/>
             </Typography>
           </IconHolder>
-          <Typography variant="h5"><b>Lessons for everything</b></Typography>
+          <Typography variant="h5" color="white" sx={{maxWidth: 200, marginTop: '0.5em', marginBottom: '0.5em'}}><b>Lessons for everything</b></Typography>
+          <Typography variant="body2" color="white" sx={{maxWidth: 250}}>We’ve written extensive documentation for what you need to know for COP3530.</Typography>
         </Feature>
       </FeatureHolder>
     </Holder>
