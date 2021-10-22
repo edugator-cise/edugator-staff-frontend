e8bbb6a660b80b4976dbb7871ba47332cfaf2b57
joshuadargan <joshuacdargan@gmail.com>
2021-10-22 11:15:33 -0400

    fleshing out the test case

--- src/pages/ProblemEditor/TestEditor/TestCase.tsx
+++ src/pages/ProblemEditor/TestEditor/TestCase.tsx
@@ -1,25 +1,156 @@
 import {
-  Button,
   Card,
   CardActions,
   CardContent,
   Typography,
+  Stack,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  RadioGroup,
 } from "@mui/material";
+import { TestCaseField, TestCaseVisibility } from "./TestCase.utils";
+import ConfirmDelete from "../../../shared/ConfirmDelete";
+import Radio from "@mui/material/Radio";
+import { FormTextField } from "../../../shared/FormTextField";
+import { Field } from "formik";
 
 interface TestCaseProps {
   index: number;
+  testCase: TestCaseField;
+  deleteFn: any;
+  setFieldValue: any;
 }
 
+// input: string;
+// expectedOutput: string;
+// hint: string;
+// visibility: TestCaseVisibility;
+
 export const TestCase = (props: TestCaseProps) => {
+  const name = `testCases.${props.index}`;
   return (
     <Card sx={{ minWidth: 275 }} key={props.index}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }}>
-          Test Case {props.index + 1}
-        </Typography>
+        <Stack direction="column" spacing={2}>
+          <Typography sx={{ fontSize: 14 }}>
+            Test Case {props.index + 1}
+          </Typography>
+          <Field
+            title="Input"
+            label="Input"
+            value={props.testCase.input}
+            placeholder="Enter test case input"
+            name={`${name}.input`}
+            multiline
+            required
+            onChange={(event: any) => {
+              props.setFieldValue(`${name}.input`, event.currentTarget.value);
+            }}
+            component={FormTextField}
+          />
+          <Field
+            title="Output"
+            label="Output"
+            value={props.testCase.expectedOutput}
+            placeholder="Enter expected output"
+            name={`${name}.expectedOutput`}
+            multiline
+            required
+            onChange={(event: any) => {
+              props.setFieldValue(
+                `${name}.expectedOutput`,
+                event.currentTarget.value
+              );
+            }}
+            component={FormTextField}
+          />
+          <Stack>
+            <FormControl component="fieldset">
+              <FormLabel component="legend">Visibility</FormLabel>
+              <RadioGroup
+                name={`${name}.visibility`}
+                row
+                value={props.testCase.visibility}
+                onChange={(event) => {
+                  props.setFieldValue(
+                    `${name}.visibility`,
+                    event.currentTarget.value
+                  );
+                }}
+              >
+                <FormControlLabel
+                  value={TestCaseVisibility.IO_VISBILE}
+                  control={<Radio />}
+                  label="Input and output visible"
+                />
+                <FormControlLabel
+                  value={TestCaseVisibility.I_VISIBLE_O_HIDDEN}
+                  control={<Radio />}
+                  label="Input visible, output hidden"
+                />
+                <FormControlLabel
+                  value={TestCaseVisibility.IO_HIDDEN}
+                  control={<Radio />}
+                  label="Input and output hidden"
+                />
+              </RadioGroup>
+            </FormControl>
+
+            {/* <div id="my-radio-group">Picked</div>
+            <div role="group" aria-labelledby="my-radio-group">
+              <label>
+                <Field
+                  type="radio"
+                  name={`testCases.${props.index}.visibility`}
+                  label="Input and output visible"
+                  value={TestCaseVisibility.IO_VISBILE}
+                  // component={<Radio />}
+                />
+                Input and output visible
+              </label>
+              <label>
+                <Field
+                  type="radio"
+                  name={`testCases.${props.index}.visibility`}
+                  label="Input visible, output hidden"
+                  value={TestCaseVisibility.I_VISIBLE_O_HIDDEN}
+                  // component={<Radio />}
+                />
+                Input visible, output hidden
+              </label>
+              <label>
+                <Field
+                  type="radio"
+                  name={`testCases.${props.index}.visibility`}
+                  label="Input and output hidden"
+                  value={TestCaseVisibility.IO_HIDDEN}
+                  // component={<Radio />}
+                />
+                Input and output hidden
+              </label>
+            </div> */}
+          </Stack>
+          <Field
+            title="Hint"
+            label="Hint"
+            value={props.testCase.hint}
+            placeholder="Enter hint"
+            name={`testCases.${props.index}.hint`}
+            multiline
+            onChange={(event: any) => {
+              props.setFieldValue(`${name}.hint`, event.currentTarget.value);
+            }}
+            component={FormTextField}
+          />
+        </Stack>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <ConfirmDelete
+          title={"Delete test case?"}
+          body={`Are you sure you want to delete test case ${props.index + 1}?`}
+          onConfirm={props.deleteFn}
+        />
       </CardActions>
     </Card>
   );
