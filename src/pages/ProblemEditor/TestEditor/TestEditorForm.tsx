20e2b7d9a3607db11dadb54a1f3788a6da17772f
joshuadargan <joshuacdargan@gmail.com>
2021-10-22 11:16:08 -0400

    improving test editor form

--- src/pages/ProblemEditor/TestEditor/TestEditorForm.tsx
+++ src/pages/ProblemEditor/TestEditor/TestEditorForm.tsx
@@ -1,27 +1,28 @@
-import React from "react";
-import { Formik, Form, Field, FieldArray, ErrorMessage } from "formik";
+import React, { useEffect } from "react";
+import { Formik, Form, FieldArray } from "formik";
 import { useDispatch } from "react-redux";
 import { useAppSelector } from "../../../app/common/hooks";
 import {
   updateTestCases,
   validateTestEditor,
 } from "../ProblemEditorContainer/problemEditorContainerSlice";
-import { Stack } from "@mui/material";
+import { Stack, Button } from "@mui/material";
 import { TestCase } from "./TestCase";
-import { generateDefaultTestCase } from "./TestCase.utils";
+import { generateDefaultTestCase, TestCaseField } from "./TestCase.utils";
 
 interface Props {
   formRef: any;
 }
 
+//https://formik.org/docs/api/fieldarray
 export const TestEditor = (props: Props) => {
   const dispatch = useDispatch();
 
-  const initialValues = useAppSelector(
+  const testCases = useAppSelector(
     (state) => state.problemEditorContainer.testCases
   );
 
-  const validate = (values: TestCaseField[]) => {
+  const validate = (values: { testCases: TestCaseField[] }) => {
     const errors: any = {};
     // for (let i = 0; i < values.length; i++)
     // {
@@ -48,39 +49,62 @@ export const TestEditor = (props: Props) => {
 
   return (
     <Formik
-      initialValues={initialValues}
-      onSubmit={(values) => {
-        dispatch(updateTestCases(values));
+      initialValues={{ testCases: testCases }}
+      onSubmit={(values: { testCases: TestCaseField[] }) => {
+        dispatch(updateTestCases(values.testCases));
       }}
       innerRef={props.formRef}
       validate={validate}
-    >
-      {({ errors, values: TestCaseField[], handleChange, handleBlur, touched }) => (
-        <Form style={{ flexGrow: 1, display: "flex", flexDirection: "column" }}>
-          <FieldArray
-            name="friends"
-            render={(arrayHelpers) => (
-              <Stack>
-                {values && values.length > 0 ? (
-                  values.map((testCase, index) => <TestCase index={index} />)
-                ) : (
-                  <button type="button" onClick={() => 
-                  {
-                    values.push(
-                      generateDefaultTestCase()
-                    );
-                    dispatch(updatedTestCases())}>
-                  }
-                    
-                    {/* show this when user has removed all friends from the list */}
-                    Add a friend
-                  </button>
-                )}
-              </Stack>
-            )}
-          />
+      render={({ values, setFieldValue }) => (
+        <Form>
+          <FieldArray name="testCases">
+            {(arrayHelpers) => {
+              const testCaseComponents = values.testCases.map(
+                (testCase, index) => {
+                  return (
+                    <TestCase
+                      key={`TestCase${index}`}
+                      index={index}
+                      testCase={testCase}
+                      deleteFn={() => {
+                        arrayHelpers.remove(index);
+                      }}
+                      setFieldValue={setFieldValue}
+                    />
+                  );
+                }
+              );
+              return (
+                <Stack
+                  direction="column"
+                  spacing={4}
+                  overflow="auto"
+                  style={{
+                    maxHeight: "500px",
+                  }}
+                >
+                  <Stack direction="column" spacing={4}>
+                    {testCaseComponents}
+                  </Stack>
+                  <Button
+                    onClick={() => {
+                      arrayHelpers.push(generateDefaultTestCase());
+                      console.log(values);
+                    }}
+                    variant="contained"
+                  >
+                    Add test case
+                  </Button>
+                </Stack>
+              );
+            }}
+          </FieldArray>
         </Form>
       )}
-    </Formik>
+    />
   );
 };
+
+// updateFn={(updated: TestCaseField) =>
+//   (values.testCases[index] = updated)
+// }
