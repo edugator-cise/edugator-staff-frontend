53f478ff59d6141cb90f246754c6cd76e0cc25b4
joshuadargan <joshuacdargan@gmail.com>
2021-10-29 17:20:05 -0400

    added the base

--- src/shared/Routes.constants.tsx
+++ src/shared/Routes.constants.tsx
@@ -2,6 +2,8 @@ export enum Routes {
   Login = "/admin/login",
   Modules = "/admin/modules",
   ProblemEditor = "/admin/problem/edit/:problemId",
+  ProblemEditorWithoutId = "/admin/problem/edit/", //To be used only when pushing to a route.
   ProblemCreator = "/admin/problem/create/:moduleId",
+  ProblemCreatorWithoutId = "/admin/problem/create/", //To be used only when pushing to a route.
   Code = "/code",
 }
