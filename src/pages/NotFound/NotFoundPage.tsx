e9613186fcc377094ee23b46e52645f6491131b5
joshuadargan <joshuacdargan@gmail.com>
2021-11-05 15:07:45 -0400

    NotFound page

---
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "@mui/material";
+import React from "react";
+import { LayoutContainer } from "../../shared/LayoutContainer";
+
+export function ModulesPage() {
+  return (
+    <LayoutContainer pageTitle={"NotFound"}>
+      <Stack></Stack>
+    </LayoutContainer>
+  );
+}
