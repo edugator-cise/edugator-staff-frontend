e3822f2bdef0106f8be77d59caa1c8c131a909fc
joshuadargan <joshuacdargan@gmail.com>
2021-11-05 15:08:48 -0400

    updated the not found page

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,11 +1,53 @@
-import { Stack } from "@mui/material";
+import { Button, Card, CardActions, CardMedia, Stack } from "@mui/material";
 import React from "react";
 import { LayoutContainer } from "../../shared/LayoutContainer";
+import EdugatorNotFound from "../../assets/EdugatorNotFound.png";
+import { useHistory } from "react-router-dom";
+import { LocalStorage } from "../../app/common/LocalStorage";
+import { Routes } from "../../shared/Routes.constants";
 
-export function ModulesPage() {
+export function NotFoundPage() {
+  const history = useHistory();
+  //history.location.pathname.indexOf("admin");
   return (
-    <LayoutContainer pageTitle={"NotFound"}>
-      <Stack></Stack>
+    <LayoutContainer pageTitle={"404: Page Not Found"}>
+      <Stack>
+        <Card>
+          <CardActions>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => history.push(Routes.Landing)}
+            >
+              Homepage
+            </Button>
+            {LocalStorage.getToken() && (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => history.push(Routes.Modules)}
+              >
+                Modules
+              </Button>
+            )}
+            {!!!LocalStorage.getToken() &&
+              history.location.pathname.indexOf("admin") !== -1 && (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => history.push(Routes.Login)}
+                >
+                  Admin Login
+                </Button>
+              )}
+          </CardActions>
+          <CardMedia
+            component="img"
+            src={EdugatorNotFound}
+            alt="page not found"
+          />
+        </Card>
+      </Stack>
     </LayoutContainer>
   );
 }
