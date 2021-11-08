8acd82b748a036e2d3cf13f0b193df37632848e5
joshuadargan <joshuacdargan@gmail.com>
2021-11-08 17:26:01 -0500

    removed layout container

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,6 +1,5 @@
 import { Button, Card, CardActions, CardMedia, Stack } from "@mui/material";
 import React from "react";
-import { LayoutContainer } from "../../shared/LayoutContainer";
 import EdugatorNotFound from "../../assets/EdugatorNotFound.png";
 import { useHistory } from "react-router-dom";
 import { LocalStorage } from "../../app/common/LocalStorage";
@@ -9,44 +8,42 @@ import { Routes } from "../../shared/Routes.constants";
 export function NotFoundPage() {
   const history = useHistory();
   return (
-    <LayoutContainer pageTitle={"404: Page Not Found"}>
-      <Stack>
-        <Card>
-          <CardActions>
+    <Stack>
+      <Card>
+        <CardActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => history.push(Routes.Landing)}
+          >
+            Homepage
+          </Button>
+          {LocalStorage.getToken() && (
             <Button
               variant="contained"
               color="primary"
-              onClick={() => history.push(Routes.Landing)}
+              onClick={() => history.push(Routes.Modules)}
             >
-              Homepage
+              Modules
             </Button>
-            {LocalStorage.getToken() && (
+          )}
+          {!!!LocalStorage.getToken() &&
+            history.location.pathname.indexOf("admin") !== -1 && (
               <Button
                 variant="contained"
                 color="primary"
-                onClick={() => history.push(Routes.Modules)}
+                onClick={() => history.push(Routes.Login)}
               >
-                Modules
+                Admin Login
               </Button>
             )}
-            {!!!LocalStorage.getToken() &&
-              history.location.pathname.indexOf("admin") !== -1 && (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={() => history.push(Routes.Login)}
-                >
-                  Admin Login
-                </Button>
-              )}
-          </CardActions>
-          <CardMedia
-            component="img"
-            src={EdugatorNotFound}
-            alt="page not found"
-          />
-        </Card>
-      </Stack>
-    </LayoutContainer>
+        </CardActions>
+        <CardMedia
+          component="img"
+          src={EdugatorNotFound}
+          alt="page not found"
+        />
+      </Card>
+    </Stack>
   );
 }
