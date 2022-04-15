c6bc14d5593dd19963a844175bf1622fa06ed75c
Poldominik <dandrzejczyn@gmail.com>
2022-04-15 11:44:27 -0400

    Added radio buttons and correct answer (on submit) to multiple choice

--- src/pages/ContentEditor/components/MultipleChoiceOption.jsx
+++ src/pages/ContentEditor/components/MultipleChoiceOption.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { EditorState, Modifier } from "draft-js";
-import "./MCOStyles.css"
+import React, { useState } from "react";
+import "./MCOStyles.css";
 
 function MultipleChoiceOption(props) {
   const [expanded, setExpanded] = useState(false);
@@ -10,6 +8,7 @@ function MultipleChoiceOption(props) {
   const [answer2, setAnswer2] = useState("");
   const [answer3, setAnswer3] = useState("");
   const [answer4, setAnswer4] = useState("");
+  const [correct, setCorrect] = useState("");
 
   //function to reset states once the question has been added to editor
   const resetValues = () => {
@@ -18,6 +17,7 @@ function MultipleChoiceOption(props) {
     setAnswer2("");
     setAnswer3("");
     setAnswer4("");
+    setCorrect("");
   };
 
   const renderModal = () => {
@@ -37,6 +37,7 @@ function MultipleChoiceOption(props) {
             setQuestion(e.target.value);
           }}
         ></input>
+
         <label className="rdw-mc-text-label" htmlFor="answer1">
           Answer 1
         </label>
@@ -51,6 +52,17 @@ function MultipleChoiceOption(props) {
           }}
           className="rdw-mc-text-input"
         ></input>
+        <input
+          type="radio"
+          name="correctAnswer"
+          htmlFor="correct"
+          value="1"
+          onClick={(e) => {
+            e.stopPropagation();
+            setCorrect(e.target.value);
+          }}
+        ></input>
+
         <label className="rdw-mc-text-label" htmlFor="answer2">
           Answer 2
         </label>
@@ -65,6 +77,17 @@ function MultipleChoiceOption(props) {
           }}
           className="rdw-mc-text-input"
         ></input>
+        <input
+          type="radio"
+          name="correctAnswer"
+          htmlFor="correct"
+          value="2"
+          onClick={(e) => {
+            e.stopPropagation();
+            setCorrect(e.target.value);
+          }}
+        ></input>
+
         <label className="rdw-mc-text-label" htmlFor="answer3">
           Answer 3
         </label>
@@ -79,6 +102,17 @@ function MultipleChoiceOption(props) {
           }}
           className="rdw-mc-text-input"
         ></input>
+        <input
+          type="radio"
+          name="correctAnswer"
+          htmlFor="correct"
+          value="3"
+          onClick={(e) => {
+            e.stopPropagation();
+            setCorrect(e.target.value);
+          }}
+        ></input>
+
         <label className="rdw-mc-text-label" htmlFor="answer4">
           Answer 4
         </label>
@@ -93,11 +127,37 @@ function MultipleChoiceOption(props) {
           }}
           className="rdw-mc-text-input"
         ></input>
+        <input
+          type="radio"
+          name="correctAnswer"
+          htmlFor="correct"
+          value="4"
+          onClick={(e) => {
+            e.stopPropagation();
+            setCorrect(e.target.value);
+          }}
+        ></input>
+
         <span className="rdw-mc-modal-btn-section">
           <button
             className="rdw-mc-modal-btn"
             onClick={() => {
-              props.insertMC(question, "A) " + answer1, "B) " + answer2, "C) " + answer3, "D) " + answer4);
+              console.log(
+                "Print MC Data:",
+                question,
+                answer1,
+                answer2,
+                answer3,
+                answer4,
+                correct
+              );
+              props.insertMC(
+                question,
+                "A) " + answer1,
+                "B) " + answer2,
+                "C) " + answer3,
+                "D) " + answer4
+              );
               resetValues();
             }}
           >
