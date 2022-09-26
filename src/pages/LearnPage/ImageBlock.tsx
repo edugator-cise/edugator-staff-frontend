4f0f101c01b2868515099744a1db3f6e829a8a9c
digitdustin <dustinkarp52@gmail.com>
2022-09-26 11:31:40 -0400

    fixed linting issues

--- src/pages/LearnPage/ImageBlock.tsx
+++ src/pages/LearnPage/ImageBlock.tsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import { Box, Grid, Typography } from '@mui/material'
-import styled from '@emotion/styled'
-import theme from '../../shared/theme'
-
 interface ImageBlockProps {
-    src: string,
-    caption: string,
-    height: string,
-    width: string,
-    alignment: string
+  src: string;
+  caption: string;
+  height: string;
+  width: string;
+  alignment: string;
 }
 
-function ImageBlock(props : ImageBlockProps) {
-  let alignment = props.alignment === "none" ? "center" : props.alignment === "left" ? "flex-start" : "flex-end";
+function ImageBlock(props: ImageBlockProps) {
+  let alignment =
+    props.alignment === "none"
+      ? "center"
+      : props.alignment === "left"
+      ? "flex-start"
+      : "flex-end";
 
   return (
-      <>
-        <img style={{alignSelf: alignment, height: props.height, width: props.width}} src={props.src} />
-        <p style={{color: 'grey', alignSelf: 'center', fontFamily: 'Inter'}}>{props.caption}</p>
-      </>
-  )
+    <>
+      <img
+        style={{
+          alignSelf: alignment,
+          height: props.height,
+          width: props.width,
+        }}
+        alt={props.caption}
+        src={props.src}
+      />
+      <p style={{ color: "grey", alignSelf: "center", fontFamily: "Inter" }}>
+        {props.caption}
+      </p>
+    </>
+  );
 }
 
-export default ImageBlock
+export default ImageBlock;
