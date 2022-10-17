e54b0330b30756612f63e5dc31a88dcfb22ea525
Dustin Karp <42528066+digitdustin@users.noreply.github.com>
2022-10-17 11:31:16 -0400

    Logo change and bugfixes (#191)
    * fixed bug #184
    
    * fixed bug #180
    
    * replaced old logo
    
    * adjusted header height
    
    * added scroll to top for footer links
    
    * updated graphic and sidebar logic
    
    * removing never used styles for CI check
    
    Co-authored-by: kapooramanpreet <kapooramanpreet@ufl.edu>

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -5,17 +5,30 @@ import theme from "./theme";
 import { Link } from "react-router-dom";
 import { Routes } from "./Routes.constants";
 
-
 const FooterHolder = styled("div")({
   width: "100%",
-  height: 50,
+  height: 100,
   backgroundColor: theme.palette.primary.dark,
   display: "flex",
-  justifyContent: "center",
+  justifyContent: "space-between",
+  paddingTop: 20,
+  paddingBottom: 20,
   alignItems: "center",
   [theme.breakpoints.down("lg")]: {
-    height: 80
-  }
+    height: "auto",
+    flexDirection: "column",
+  },
+});
+
+const LinkHolder = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-end",
+  marginRight: "50px",
+  [theme.breakpoints.down("lg")]: {
+    alignItems: "center",
+    marginRight: 0,
+  },
 });
 
 function Footer() {
@@ -26,27 +39,60 @@ function Footer() {
       <Typography
         variant={md ? "body2" : "body1"}
         color={theme.palette.primary.light}
-        style={{ marginRight: "8px" }}
+        style={
+          md
+            ? { maxWidth: "70%" }
+            : { textAlign: "left", marginLeft: "50px", maxWidth: "40%" }
+        }
       >
         © Copyright 2022 Edugator @ University of Florida Research Foundation,
         Inc. All Commercial Rights Reserved.
       </Typography>
 
-      <Link to={Routes.PrivacyNotice} style={{ textDecoration: "none" }}>
-        <Typography variant={md ? "body2" : "body1"} color={theme.palette.primary.light}>
-          Privacy Terms⠀|⠀
-        </Typography>
-      </Link>
-      <Link to={Routes.FERPA} style={{ textDecoration: "none" }}>
-        <Typography variant={md ? "body2" : "body1"} color={theme.palette.primary.light}>
-          FERPA⠀|⠀
-        </Typography>
-      </Link>
-      <Link to={Routes.TermsOfUse} style={{ textDecoration: "none" }}>
-        <Typography variant={md ? "body2" : "body1"} color={theme.palette.primary.light}>
-          Terms of use⠀|⠀Team
-        </Typography>
-      </Link>
+      <LinkHolder>
+        <Link
+          to={Routes.PrivacyNotice}
+          style={{ textDecoration: "none" }}
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+        >
+          <Typography
+            variant={md ? "body2" : "body1"}
+            color={theme.palette.primary.light}
+          >
+            Privacy Terms
+          </Typography>
+        </Link>
+        <Link
+          to={Routes.TermsOfUse}
+          style={{ textDecoration: "none" }}
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+        >
+          <Typography
+            variant={md ? "body2" : "body1"}
+            color={theme.palette.primary.light}
+          >
+            Terms of use
+          </Typography>
+        </Link>
+        <Link
+          to={Routes.FERPA}
+          style={{ textDecoration: "none" }}
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+        >
+          <Typography
+            variant={md ? "body2" : "body1"}
+            color={theme.palette.primary.light}
+          >
+            FERPA
+          </Typography>
+        </Link>
+      </LinkHolder>
     </FooterHolder>
   );
 }
