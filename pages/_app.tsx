dac454649b5b6dc36c97fa3476592af9fd85c079
digitdustin <dustinkarp52@gmail.com>
2023-01-16 18:51:29 -0500

    fix styling files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/index.css'
-import type { AppProps } from 'next/app'
+import "../styles/index.css";
+import "../styles/App.css";
+import theme from "../src/shared/theme";
+
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,28 +5,37 @@ import LandingHome from "../src/pages/LandingPage/LandingHome";
 import LandingTopics from "../src/pages/LandingPage/LandingTopics";
 import { IModuleBase } from "../src/shared/types";
 import VerticalNavigation from "../src/shared/VerticalNavigation";
-import '../styles/App.module.css'
+import { ThemeProvider, StyledEngineProvider } from "@mui/system";
+import theme from "src/shared/theme";
+import "../styles/App.module.css";
 
-function LandingPage({modules}: InferGetStaticPropsType<typeof getServerSideProps>) {
+function LandingPage({
+  modules,
+}: InferGetStaticPropsType<typeof getServerSideProps>) {
   return (
-    <>
-      <VerticalNavigation light modules={modules.map(value => value.name)}></VerticalNavigation>
-      <LandingHome />
-      <LandingFeatures />
-      <LandingTopics />
-    </>
-  )
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <VerticalNavigation
+          light
+          modules={modules.map((value) => value.name)}
+        ></VerticalNavigation>
+        <LandingHome />
+        <LandingFeatures />
+        <LandingTopics />
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 }
 
 export const getServerSideProps = async () => {
-  const { data }: { data: IModuleBase[]} = await apiClient.get("v1/module");
+  const { data }: { data: IModuleBase[] } = await apiClient.get("v1/module");
   const sortedModules = data.sort(
     (valueA, valueB) => valueA.number - valueB.number
-  )
+  );
   return {
     props: {
-      modules: sortedModules
-    }
-  }
-}
+      modules: sortedModules,
+    },
+  };
+};
 export default LandingPage;
