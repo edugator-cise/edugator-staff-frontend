c5555b9598f9ab7ed39824b54c31b9f6acea0c23
Marc Diaz <marcgabe15@gmail.com>
2023-01-16 20:00:35 -0800

    add error return for useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -8,9 +8,10 @@ export enum FetchStatus {
 }
 export const useFetch = <T>(
   url: string
-): { status: FetchStatus; data: T | undefined } => {
+): { status: FetchStatus; data: T | undefined; error: Error | undefined } => {
   const [status, setStatus] = useState<FetchStatus>(FetchStatus.loading);
   const [data, setData] = useState<T | undefined>(undefined);
+  const [error, setError] = useState<Error | undefined>(undefined);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,7 +25,8 @@ export const useFetch = <T>(
       })
       .catch((e) => {
         setStatus(FetchStatus.failed);
+        setError(e);
       });
   });
-  return { status, data };
+  return { status, data, error };
 };
