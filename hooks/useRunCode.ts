cad1ffaf6435dd926a7bbe8013c30cbce91a1039
Amanpreet Kapoor <kapooramanpreet@ufl.edu>
2023-04-03 09:41:47 -0400

    bug fixed in code submit feature (#225)

--- hooks/useRunCode.ts
+++ hooks/useRunCode.ts
@@ -153,6 +153,7 @@ export const useRunCode = (locationState: string) => {
       );
       setActiveTab(2);
       setIsSubmissionRunning(false);
+      setSubmissionOutput(data);
     } catch (error: any) {
       setIsSubmissionRunning(false);
       toast.error(error.message || "Something went wrong");
