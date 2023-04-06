c300d0732fffe9122cf656200151d877e12fe44d
Dustin <dustinkarp52@gmail.com>
2023-04-06 13:10:34 -0400

    added cursor

--- components/LegalLayout.tsx
+++ components/LegalLayout.tsx
@@ -19,7 +19,7 @@ const LegalLayout = ({
         <div className="max-w-7xl w-full px-4 sm:px-6 lg:px-8 grid items-center h-16 grid-cols-6 space-x-3">
           <Link href="/">
             <div className="flex col-span-3 justify-start items-center space-x-2">
-              <div className="w-12 h-12 min-w-[3rem] p-1 flex items-center">
+              <div className="cursor-pointer w-12 h-12 min-w-[3rem] p-1 flex items-center">
                 <EdugatorLogo />
               </div>
               <h1
@@ -27,7 +27,7 @@ const LegalLayout = ({
                   WebkitMaskImage:
                     "linear-gradient(to bottom, rgba(0,0,0,1) 50%, rgba(0,0,0,0.3) 100%)",
                 }}
-                className={`text-xl font-semibold transition font-ambit text-slate-50`}
+                className={`text-xl cursor-pointer font-semibold transition font-ambit text-slate-50`}
               >
                 Edugator
               </h1>
